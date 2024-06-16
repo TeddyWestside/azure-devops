21f4c82832263c8dbfa61c0c03aebd105523c7fe
Carsten Mußenbrock <carsten.mussenbrock@arvato-scs.com>
2024-06-16 14:58:15 +0200

    adjust hello to udacity starter code

--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
 import click
 
-
-@click.command(help="This is a simple program that greets you")
-@click.option("--name", prompt="Your Name", help="Need name")
-@click.option("--color", prompt="Your Color", help="This is your color")
+@click.command(help="This is just a hello app. It does nothing.")
+@click.option("--name", prompt="I need your name", help="Need name")
+@click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
     if name == "Thor":
-        click.echo("Hello, Thor. You are always red.")
-        click.echo(click.style(f"Hello, {name}. You are always {color}.", fg="red"))
+        click.echo("Thor, you are always red.")
+        click.echo(click.style(f"Hello {name}!", fg="red"))
     else:
-        click.echo(click.style(f"Hello, {name}. You are always {color}.", fg=f"{color}"))
+        click.echo(f"Your color is {color}!")
+        click.echo(click.style(f"Hello {name}!", fg=color))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     hello()
